a68b2dfba48bbb559cb7a31ee95d9705b5c1e91e
zssm <31382580+zssm@users.noreply.github.com>
2018-10-20 02:32:41 +1100

    Add files via upload

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-
+#imputation, fill NA with mean or median
 def impute(df, by, method='mean'):
     if method == 'mean':
         return df.fillna(by.mean())
@@ -10,20 +10,20 @@ def impute(df, by, method='mean'):
     else:
         raise ValueError("Imputation method not allowed!\n - Please choose from ['mean','median']")
 
-
+# load files from specific path
 def load_files(dir_name):
     dfs = []
     for file in os.listdir(dir_name):
         dfs.append(pd.read_csv(dir_name + file))
     return dfs
 
-
+# change long format to wide format
 def long2wide(dfs, col, value, index=None):
     for i in range(len(dfs)):
         dfs[i] = dfs[i].pivot(index=index, columns=col, values=value)
     return dfs
 
-
+# calculate mean, maximum,and minimum values of features from dataframe and concatenate together
 def feature_extract(dfs, static_col):
     mean = pd.DataFrame()
     maximum = pd.DataFrame()
@@ -40,7 +40,7 @@ def feature_extract(dfs, static_col):
     minimum = minimum.drop(static_col, axis=1).add_suffix('_min')
     return pd.concat([static, mean, maximum, minimum], axis=1, sort=False)
 
-
+#find categorical and numerical features and concatenate them together.
 def non0var(df):
     categorical = df.select_dtypes(include='object')
     numerical = df.select_dtypes(exclude='object')
@@ -50,10 +50,14 @@ def non0var(df):
 
 
 def synchronize(train, validation, test):
+    # remove rows which not exists in train set and validation set.
     test = test.drop(list(set(test).difference(set(train))), axis=1)
+
     test = test.drop(list(set(test).difference(set(validation))), axis=1)
+    # remove rows which not exists in train set and test set.
     validation = validation.drop(list(set(validation).difference(set(train))), axis=1)
     validation = validation.drop(list(set(validation).difference(set(test))), axis=1)
+    # remove rows which not exists in test set and validation set.
     train = train.drop(list(set(train).difference(set(test))), axis=1)
     train = train.drop(list(set(train).difference(set(validation))), axis=1)
     return train, validation, test
